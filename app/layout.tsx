fd0022cecfa37e541cb7cbbf3b743521fe9cc65a
Ahsan Adil <muhammadmahsanadil@gmail.com>
2025-07-28 03:01:48 +0300

    update the title for my desc gen

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Desc | Tags Gen',
+  description: 'Created by Ahsan Adil',
+  generator: 'Description generator by ahsan',
 }
 
 export default function RootLayout({
